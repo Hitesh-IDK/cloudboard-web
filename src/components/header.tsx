e4912997b720e6cffce10f6234a160d82ec07e38
HiteshIDK <10hiteshparmar@gmail.com>
2024-05-20 19:05:31 +0530

    added inter font, added files for header component

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./header.module.css";
+
+export default function () {
+  return (
+    <header>
+      <h3 className={styles.header_title}>CloudBoard</h3>
+    </header>
+  );
+}
