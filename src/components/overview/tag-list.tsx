a5341f7c07d27692d7a15bc85916f2ef49d760c1
HiteshIDK <10hiteshparmar@gmail.com>
2024-05-30 17:25:30 +0530

    added icons for all the tag types

--- src/components/overview/tag-list.tsx
+++ src/components/overview/tag-list.tsx
@@ -4,28 +4,40 @@ import {
   TagCloseButton,
   TagLabel,
   TagLeftIcon,
+  TagRightIcon,
 } from "@chakra-ui/react";
 import styles from "./tag-list.module.css";
+import { AddIcon } from "@chakra-ui/icons";
 
 export default function TagList(): JSX.Element {
   return (
     <div className={styles.list__container}>
       <h3 className={styles.list__title}>Tags</h3>
 
-      <HStack gap={"0.5rem"}>
+      <HStack spacing={3}>
         <Tag
           size="lg"
           variant="solid"
           backgroundColor={"#47c7ce"}
           borderRadius={"full"}
+          style={{ cursor: "pointer" }}
         >
           <TagLabel>Label</TagLabel>
           <TagCloseButton />
         </Tag>
 
-        <Tag size={"lg"} variant="solid" borderRadius={"full"}>
-          <TagLeftIcon boxSize="12px" />
+        <Tag
+          size={"lg"}
+          variant="solid"
+          borderRadius={"full"}
+          style={{ cursor: "pointer" }}
+        >
           <TagLabel>Label</TagLabel>
+          <TagRightIcon
+            boxSize="12px"
+            as={AddIcon}
+            style={{ cursor: "pointer" }}
+          />
         </Tag>
       </HStack>
     </div>
