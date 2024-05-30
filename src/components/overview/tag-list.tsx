961f4ab88b21ccd163a38ab2ee021c28bf1bf5cd
HiteshIDK <10hiteshparmar@gmail.com>
2024-05-30 17:03:50 +0530

    changes to tag list

--- src/components/overview/tag-list.tsx
+++ src/components/overview/tag-list.tsx
@@ -1,19 +1,33 @@
-import { Tag, TagCloseButton, TagLabel } from "@chakra-ui/react";
+import {
+  HStack,
+  Tag,
+  TagCloseButton,
+  TagLabel,
+  TagLeftIcon,
+} from "@chakra-ui/react";
 import styles from "./tag-list.module.css";
 
 export default function TagList(): JSX.Element {
   return (
     <div className={styles.list__container}>
       <h3 className={styles.list__title}>Tags</h3>
-      <Tag
-        size="lg"
-        variant="solid"
-        backgroundColor={"#47c7ce"}
-        borderRadius={"full"}
-      >
-        <TagLabel>Label</TagLabel>
-        <TagCloseButton />
-      </Tag>
+
+      <HStack gap={"0.5rem"}>
+        <Tag
+          size="lg"
+          variant="solid"
+          backgroundColor={"#47c7ce"}
+          borderRadius={"full"}
+        >
+          <TagLabel>Label</TagLabel>
+          <TagCloseButton />
+        </Tag>
+
+        <Tag size={"lg"} variant="solid" borderRadius={"full"}>
+          <TagLeftIcon boxSize="12px" />
+          <TagLabel>Label</TagLabel>
+        </Tag>
+      </HStack>
     </div>
   );
 }
